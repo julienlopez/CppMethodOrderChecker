e29354376f56a4678778a721c72372881b93b961
julien lopez <julien.lopez51@gmail.com>
2016-10-27 18:14:50 +0200

    starting to work on comparing method in the two files

--- lib/analyzer.cpp
+++ lib/analyzer.cpp
@@ -3,6 +3,7 @@
 #include "sourceparser.hpp"
 
 #include <boost/filesystem.hpp>
+#include <boost/iterator/zip_iterator.hpp>
 
 #include <iostream>
 
@@ -14,20 +15,52 @@ Analyzer::Analyzer(const boost::filesystem::path& file)
         throw std::runtime_error("can't find both source file and header file");
     std::tie(m_class_name, m_header_methods) = HeaderParser(m_header_file).run();
 
-    std::cout << "Class found: " << m_class_name << std::endl;
-    std::cout << m_header_methods.size() << std::endl;
-    std::copy(begin(m_header_methods), end(m_header_methods), std::ostream_iterator<std::string>(std::cout, "\n"));
-    std::cout << std::endl;
+    // std::cout << "Class found: " << m_class_name << std::endl;
+    // std::cout << m_header_methods.size() << std::endl;
+    // std::copy(begin(m_header_methods), end(m_header_methods), std::ostream_iterator<std::string>(std::cout, "\n"));
+    // std::cout << std::endl;
 
     m_source_methods = SourceParser(m_class_name, m_source_file).run();
 
-    std::cout << m_source_methods.size() << std::endl;
-    std::copy(begin(m_source_methods), end(m_source_methods), std::ostream_iterator<std::string>(std::cout, "\n"));
-    std::cout << std::endl;
+    // std::cout << m_source_methods.size() << std::endl;
+    // std::copy(begin(m_source_methods), end(m_source_methods), std::ostream_iterator<std::string>(std::cout, "\n"));
+    // std::cout << std::endl;
 
     parseCompareBothMethods();
 }
 
-void Analyzer::parseCompareBothMethods()
+bool Analyzer::parseCompareBothMethods() const
 {
+    if(m_source_methods.size() != m_header_methods.size())
+    {
+        std::cout << "Not the same number of methods found." << std::endl;
+        return true;
+    }
+    bool error_found = false;
+    for(auto it = boost::make_zip_iterator(boost::make_tuple(begin(m_header_methods), begin(m_source_methods)));
+        it != boost::make_zip_iterator(boost::make_tuple(end(m_header_methods), end(m_source_methods))); ++it)
+    {
+        //std::cout << it->get<0>() << " =?= " << it->get<1>() << std::endl;
+        if(cleanMethodSignature(it->get<0>()) != cleanMethodSignature(it->get<1>()))
+        {
+            error_found = true;
+            std::cout << "differences found:\n\t" << it->get<0>() << "\n\t" << it->get<1>() << std::endl;
+        }
+    }
+    if(!error_found)
+        std::cout << "nothing to whine about" << std::endl;
+    return !error_found;
+}
+
+std::string Analyzer::cleanMethodSignature(std::string line)
+{
+    if(Utils::startsWith(line, "auto ") && Utils::contains(line, "->"))
+    {
+        const auto pos = line.find("->");
+        assert(pos != std::string::npos);
+        const auto method = Utils::trim(line.substr(4, pos - 4));
+        const auto type = Utils::trim(line.substr(pos + 2));
+        return type + " " + method;
+    }
+    return line;
 }
--- lib/analyzer.hpp
+++ lib/analyzer.hpp
@@ -10,10 +10,12 @@ public:
     const boost::filesystem::path m_header_file;
     const boost::filesystem::path m_source_file;
 
+    bool parseCompareBothMethods() const;
+
 private:
     std::string m_class_name;
     Utils::StringList_t m_header_methods;
     Utils::StringList_t m_source_methods;
 
-    void parseCompareBothMethods();
+    static std::string cleanMethodSignature(std::string line);
 };
